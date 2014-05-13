4d941fd53f59e711563537a222e63790c70e506a
marcelr <marcelrienks@gmail.com>
2014-05-13 10:38:38 +0200

    Bootstrap * added bootstrap theme to Content * Added BundleConfig
    Signed-off-by: marcelr <marcelrienks@gmail.com>

--- Scrummage/App_Start/BundleConfig.cs
+++ Scrummage/App_Start/BundleConfig.cs
@@ -3,6 +3,7 @@ using System.Web.Optimization;
 
 namespace Scrummage {
 	public class BundleConfig {
+		
 		// For more information on Bundling, visit http://go.microsoft.com/fwlink/?LinkId=254725
 		public static void RegisterBundles(BundleCollection bundles) {
 			bundles.Add(new ScriptBundle("~/bundles/jquery").Include(
