f32547afe36b015effdc099846060390b5ea3dd7
marcelr <marcelrienks@gmail.com>
2014-05-14 14:09:21 +0200

    Nav Bar * Added a navigation bar to layout page with links to Members and Roles
    Signed-off-by: marcelr <marcelrienks@gmail.com>

--- Scrummage/App_Start/BundleConfig.cs
+++ Scrummage/App_Start/BundleConfig.cs
@@ -5,7 +5,9 @@ namespace Scrummage {
 
 		// For more information on Bundling, visit http://go.microsoft.com/fwlink/?LinkId=254725
 		public static void RegisterBundles(BundleCollection bundles) {
-			bundles.Add(new ScriptBundle("~/bundles/jquery").Include("~/Scripts/jquery-{version}.js"));
+			bundles.Add(new ScriptBundle("~/bundles/jquery").Include(
+				"~/Scripts/jquery-{version}.js",
+				"~/Scripts/bootstrap.js"));
 
 			bundles.Add(new ScriptBundle("~/bundles/jqueryval").Include(
 				"~/Scripts/jquery.validate.unobtrusive*",
